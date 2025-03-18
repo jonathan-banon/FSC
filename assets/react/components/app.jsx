34e241d16db7a59c908cacad9ee5b9da0c19c083
jonathan-banon <j.banon01@gmail.com>
2025-03-18 18:23:25 +0100

    feat : login-form html

--- assets/react/components/app.jsx
+++ assets/react/components/app.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-
 const App = () => {
     return (
-        <div className="home-bg">
+        <div className='d-flex justify-content-center align-items-center bg-home'>
+            <form className='form-login'>
+                <div className='d-flex flex-column'>
+                    <label>Mail</label>
+                    <input type="text" placeholder=" " />
+                    <label>Mot de passe</label>
+                    <input type="text" placeholder=" " />
+                    <div className='d-flex flex-column'>
+                        <button>Me connecter</button>
+                        <button>M'inscrire</button>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
