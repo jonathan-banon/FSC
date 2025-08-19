c23b9355b9789d9865dc2a4e4f8a80acb04a1cee
jonathan-banon <j.banon01@gmail.com>
2025-08-19 17:27:46 +0200

    feat: front sign-in

--- assets/react/components/SignIn.jsx
+++ assets/react/components/SignIn.jsx
@@ -43,43 +43,114 @@ const SignIn = () => {
             console.log("data => ", data)
             setMessage(data.message);
         } catch (error) {
-            console.log("error => ",error);
+            console.log("error => ", error);
             setMessage("Erreur lors de l'inscription.");
         }
     };
 
     return (
-        <div className='d-flex justify-content-center align-items-center bg-home'>
+        <div className='d-flex justify-content-center align-items-center min-vh-100 bg-home p-3'>
             <form className='form-login' onSubmit={handleSubmit}>
-                <div>
-                    <label>Prénom</label>
-                    <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
+                <div className='mt-2'>
+                    <div className="row mb-4">
+                        <div className="col-md-6">
+                            <div className="did-floating-label-content">
+                                <input
+                                    type="text"
+                                    className="did-floating-input"
+                                    placeholder=" "
+                                    name="firstName"
+                                    value={formData.firstName}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label className="did-floating-label">Prénom</label>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="did-floating-label-content">
+                                <input
+                                    type="text"
+                                    className="did-floating-input"
+                                    placeholder=" "
+                                    name="lastName"
+                                    value={formData.lastName}
+                                    onChange={handleChange}
+                                    required
+                                />
+                                <label className="did-floating-label">Nom</label>
+                            </div>
+                        </div>
+                    </div>
 
-                    <label>Nom</label>
-                    <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} required />
+                    <div className="did-floating-label-content mb-4">
+                        <input
+                            type="text"
+                            className="did-floating-input"
+                            placeholder=" "
+                            name="club"
+                            value={formData.club}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label className="did-floating-label">Nom de Club</label>
+                    </div>
+
+                    <div className="did-floating-label-content mb-4">
+                        <input
+                            type="email"
+                            className="did-floating-input"
+                            placeholder=" "
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label className="did-floating-label">Email</label>
+                    </div>
+
+                    <div className="did-floating-label-content mb-4">
+                        <input
+                            type="password"
+                            className="did-floating-input"
+                            placeholder=" "
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label className="did-floating-label">Mot de passe</label>
+                    </div>
+
+                    <div className="did-floating-label-content mb-3">
+                        <input
+                            type="password"
+                            className="did-floating-input"
+                            placeholder=" "
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                            required
+                        />
+                        <label className="did-floating-label">Confirmation mot de passe</label>
+                    </div>
+
+                    {message && (
+                        <div className="alert alert-info text-center mb-3" role="alert">
+                            {message}
+                        </div>
+                    )}
                 </div>
-                <div>
-                    <label>Nom de Club</label>
-                    <input type="text" name="club" value={formData.club} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Mot de passe</label>
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} required />
-                </div>
-                <div>
-                    <label>Validation de mot de passe</label>
-                    <input type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} required />
-                </div>
-                {message && <p>{message}</p>}
-                <div>
-                    <Link to="/">
-                        <button type="button">Retour</button>
+
+                <div className="d-grid gap-2">
+                    <button className="bg-dark rounded-pill text-white mb-3" type="submit">
+                        Valider l'inscription
+                    </button>
+                    <Link to="/" className='text-center'>
+                        <button className="bg-white rounded-pill" type="button">
+                            Retour à la connexion
+                        </button>
                     </Link>
-                    <button type="submit">Valider</button>
                 </div>
             </form>
         </div>
